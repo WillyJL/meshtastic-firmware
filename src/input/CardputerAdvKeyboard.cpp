a7928b1d3363aa9b4523ef7d56361c21b87efdf3
WillyJL <me@willyjl.dev>
2025-09-17 03:40:23 +0200

    Cardputer Adv: Port PR 7989 Keyboard Improvements

--- src/input/CardputerAdvKeyboard.cpp
+++ src/input/CardputerAdvKeyboard.cpp
@@ -7,8 +7,6 @@
 #define _TCA8418_ROWS 7
 #define _TCA8418_NUM_KEYS 56
 
-#define _TCA8418_MULTI_TAP_THRESHOLD 1500
-
 using Key = TCA8418KeyboardBase::TCA8418Key;
 
 constexpr uint8_t modifierShiftKey = 7 - 1; // keynum -1
@@ -25,70 +23,74 @@ constexpr uint8_t modifierAltKey = 12 - 1;
 
 // Num chars per key, Modulus for rotating through characters
 // https://m5stack-doc.oss-cn-shenzhen.aliyuncs.com/1178/Sch_M5CardputerAdv_v1.0_2025_06_20_17_19_58_page_02.png
-static uint8_t CardputerAdvTapMod[_TCA8418_NUM_KEYS] = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-                                                        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-                                                        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+static const uint8_t CardputerAdvTapMod[_TCA8418_NUM_KEYS] = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                                                              3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                                                              3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
 
-static unsigned char CardputerAdvTapMap[_TCA8418_NUM_KEYS][3] = {{'`', '~', Key::ESC},
-                                                                 {Key::TAB, 0x00, 0x00},
-                                                                 {0x00, 0x00, 0x00}, // Fn
-                                                                 {0x00, 0x00, 0x00}, // ctrl
-                                                                 {'1', '!', 0x00},
-                                                                 {'q', 'Q', Key::REBOOT},
-                                                                 {0x00, 0x00, 0x00}, // shift
-                                                                 {0x00, 0x00, 0x00}, // opt
-                                                                 {'2', '@', 0x00},
-                                                                 {'w', 'W', 0x00},
-                                                                 {'a', 'A', 0x00},
-                                                                 {0x00, 0x00, 0x00}, // alt
-                                                                 {'3', '#', 0x00},
-                                                                 {'e', 'E', 0x00},
-                                                                 {'s', 'S', 0x00},
-                                                                 {'z', 'Z', 0x00},
-                                                                 {'4', '$', 0x00},
-                                                                 {'r', 'R', 0x00},
-                                                                 {'d', 'D', 0x00},
-                                                                 {'x', 'X', 0x00},
-                                                                 {'5', '%', 0x00},
-                                                                 {'t', 'T', 0x00},
-                                                                 {'f', 'F', 0x00},
-                                                                 {'c', 'C', 0x00},
-                                                                 {'6', '^', 0x00},
-                                                                 {'y', 'Y', 0x00},
-                                                                 {'g', 'G', Key::GPS_TOGGLE},
-                                                                 {'v', 'V', 0x00},
-                                                                 {'7', '&', 0x00},
-                                                                 {'u', 'U', 0x00},
-                                                                 {'h', 'H', 0x00},
-                                                                 {'b', 'B', Key::BT_TOGGLE},
-                                                                 {'8', '*', 0x00},
-                                                                 {'i', 'I', 0x00},
-                                                                 {'j', 'J', 0x00},
-                                                                 {'n', 'N', 0x00},
-                                                                 {'9', '(', 0x00},
-                                                                 {'o', 'O', 0x00},
-                                                                 {'k', 'K', 0x00},
-                                                                 {'m', 'M', Key::MUTE_TOGGLE},
-                                                                 {'0', ')', 0x00},
-                                                                 {'p', 'P', Key::SEND_PING},
-                                                                 {'l', 'L', 0x00},
-                                                                 {',', '<', Key::LEFT},
-                                                                 {'_', '-', 0x00},
-                                                                 {'[', '{', 0x00},
-                                                                 {';', ':', Key::UP},
-                                                                 {'.', '>', Key::DOWN},
-                                                                 {'=', '+', 0x00},
-                                                                 {']', '}', 0x00},
-                                                                 {'\'', '"', 0x00},
-                                                                 {'/', '?', Key::RIGHT},
-                                                                 {Key::BSP, 0x00, 0x00},
-                                                                 {'\\', '|', 0x00},
-                                                                 {Key::SELECT, 0x00, 0x00}, // Enter
-                                                                 {' ', ' ', ' '}};          // Space
+static const uint8_t CardputerAdvTapMap[_TCA8418_NUM_KEYS][3] = {
+    {'`', '~', Key::ESC},
+    {Key::TAB, 0x00, 0x00},
+    {0x00, 0x00, 0x00}, // Fn
+    {0x00, 0x00, 0x00}, // ctrl
+    {'1', '!', 0x00},
+    {'q', 'Q', Key::REBOOT},
+    {0x00, 0x00, 0x00}, // shift
+    {0x00, 0x00, 0x00}, // opt
+    {'2', '@', 0x00},
+    {'w', 'W', 0x00},
+    {'a', 'A', 0x00},
+    {0x00, 0x00, 0x00}, // alt
+    {'3', '#', 0x00},
+    {'e', 'E', 0x00},
+    {'s', 'S', 0x00},
+    {'z', 'Z', 0x00},
+    {'4', '$', 0x00},
+    {'r', 'R', 0x00},
+    {'d', 'D', 0x00},
+    {'x', 'X', 0x00},
+    {'5', '%', 0x00},
+    {'t', 'T', 0x00},
+    {'f', 'F', 0x00},
+    {'c', 'C', 0x00},
+    {'6', '^', 0x00},
+    {'y', 'Y', 0x00},
+    {'g', 'G', Key::GPS_TOGGLE},
+    {'v', 'V', 0x00},
+    {'7', '&', 0x00},
+    {'u', 'U', 0x00},
+    {'h', 'H', 0x00},
+    {'b', 'B', Key::BT_TOGGLE},
+    {'8', '*', 0x00},
+    {'i', 'I', 0x00},
+    {'j', 'J', 0x00},
+    {'n', 'N', 0x00},
+    {'9', '(', 0x00},
+    {'o', 'O', 0x00},
+    {'k', 'K', 0x00},
+    {'m', 'M', Key::MUTE_TOGGLE},
+    {'0', ')', 0x00},
+    {'p', 'P', Key::SEND_PING},
+    {'l', 'L', 0x00},
+    {',', '<', Key::LEFT},
+    {'_', '-', 0x00},
+    {'[', '{', 0x00},
+    {';', ':', Key::UP},
+    {'.', '>', Key::DOWN},
+    {'=', '+', 0x00},
+    {']', '}', 0x00},
+    {'\'', '"', 0x00},
+    {'/', '?', Key::RIGHT},
+    {Key::BSP, 0x00, 0x00},
+    {'\\', '|', 0x00},
+    {Key::SELECT, 0x00, 0x00}, // Enter
+    {' ', ' ', ' '},           // Space
+};
+
+static bool CardputerAdvHeldMap[_TCA8418_NUM_KEYS] = {};
 
 CardputerAdvKeyboard::CardputerAdvKeyboard()
-    : TCA8418KeyboardBase(_TCA8418_ROWS, _TCA8418_COLS), modifierFlag(0), last_modifier_time(0), last_key(-1), next_key(-1),
-      last_tap(0L), char_idx(0), tap_interval(0)
+    : TCA8418KeyboardBase(_TCA8418_ROWS, _TCA8418_COLS), modifierFlag(0), pressedKeysCount(0), onlyOneModifierPressed(false),
+      persistedPreviousModifier(false)
 {
     reset();
 }
@@ -98,111 +100,90 @@ void CardputerAdvKeyboard::reset(void)
     TCA8418KeyboardBase::reset();
 }
 
-// handle multi-key presses (shift and alt)
-void CardputerAdvKeyboard::trigger()
+int8_t CardputerAdvKeyboard::keyToIndex(uint8_t key)
 {
-    uint8_t count = keyCount();
-    if (count == 0)
-        return;
-    for (uint8_t i = 0; i < count; ++i) {
-        uint8_t k = readRegister(TCA8418_REG_KEY_EVENT_A + i);
-        uint8_t key = k & 0x7F;
-        if (k & 0x80) {
-            pressed(key);
-        } else {
-            released();
-            state = Idle;
-        }
-    }
-}
-
-void CardputerAdvKeyboard::pressed(uint8_t key)
-{
-    if (state == Init || state == Busy) {
-        return;
-    }
-
-    if (modifierFlag && (millis() - last_modifier_time > _TCA8418_MULTI_TAP_THRESHOLD)) {
-        modifierFlag = 0;
-    }
-
-    uint8_t next_key = 0;
+    uint8_t key_index = 0;
     int row = (key - 1) / 10;
     int col = (key - 1) % 10;
 
     if (row >= _TCA8418_ROWS || col >= _TCA8418_COLS) {
-        return; // Invalid key
+        return -1; // Invalid key
     }
 
-    next_key = row * _TCA8418_COLS + col;
-    state = Held;
-
-    uint32_t now = millis();
-    tap_interval = now - last_tap;
-
-    updateModifierFlag(next_key);
-    if (isModifierKey(next_key)) {
-        last_modifier_time = now;
-    }
-
-    if (tap_interval < 0) {
-        last_tap = 0;
-        state = Busy;
-        return;
-    }
-
-    if (next_key != last_key || tap_interval > _TCA8418_MULTI_TAP_THRESHOLD) {
-        char_idx = 0;
-    } else {
-        char_idx += 1;
-    }
-
-    last_key = next_key;
-    last_tap = now;
+    key_index = row * _TCA8418_COLS + col;
+    return key_index;
 }
 
-void CardputerAdvKeyboard::released()
+void CardputerAdvKeyboard::pressed(uint8_t key)
 {
-    if (state != Held) {
+    int8_t key_index = keyToIndex(key);
+    if (key_index < 0)
+        return;
+
+    if (CardputerAdvHeldMap[key_index]) {
         return;
     }
 
-    if (last_key < 0 || last_key >= _TCA8418_NUM_KEYS) {
-        last_key = -1;
-        state = Idle;
-        return;
+    CardputerAdvHeldMap[key_index] = true;
+    pressedKeysCount++;
+
+    uint8_t key_modifier = keyToModifierFlag(key_index);
+    if (key_modifier && pressedKeysCount == 1) {
+        onlyOneModifierPressed = true;
+    } else {
+        onlyOneModifierPressed = false;
     }
-
-    uint32_t now = millis();
-    last_tap = now;
-
-    if (CardputerAdvTapMap[last_key][modifierFlag % CardputerAdvTapMod[last_key]] == Key::BL_TOGGLE) {
-        return;
-    }
-
-    queueEvent(CardputerAdvTapMap[last_key][modifierFlag % CardputerAdvTapMod[last_key]]);
-    if (isModifierKey(last_key) == false)
-        modifierFlag = 0;
+    modifierFlag |= key_modifier;
 }
 
-void CardputerAdvKeyboard::updateModifierFlag(uint8_t key)
+void CardputerAdvKeyboard::released(uint8_t key)
+{
+    int8_t key_index = keyToIndex(key);
+    if (key_index < 0)
+        return;
+
+    if (!CardputerAdvHeldMap[key_index]) {
+        return;
+    }
+
+    queueEvent(CardputerAdvTapMap[key_index][modifierFlag % CardputerAdvTapMod[key_index]]);
+
+    CardputerAdvHeldMap[key_index] = false;
+    pressedKeysCount--;
+
+    if (onlyOneModifierPressed) {
+        onlyOneModifierPressed = false;
+        if (persistedPreviousModifier) {
+            modifierFlag = 0;
+        }
+        persistedPreviousModifier = !persistedPreviousModifier;
+    } else if (persistedPreviousModifier && pressedKeysCount == 0) {
+        modifierFlag = 0;
+        persistedPreviousModifier = false;
+    } else {
+        modifierFlag &= ~keyToModifierFlag(key_index);
+    }
+}
+
+uint8_t CardputerAdvKeyboard::keyToModifierFlag(uint8_t key)
 {
     if (key == modifierShiftKey) {
-        modifierFlag ^= modifierRightShift;
+        return modifierRightShift;
     } else if (key == modifierFnKey) {
-        modifierFlag ^= modifierFn;
+        return modifierFn;
     } else if (key == modifierCtrlKey) {
-        // modifierFlag ^= modifierCtrl;
+        // return modifierCtrl;
     } else if (key == modifierOptKey) {
-        // modifierFlag ^= modifierOpt;
+        // return modifierOpt;
     } else if (key == modifierAltKey) {
-        // modifierFlag ^= modifierAlt;
+        // return modifierAlt;
     }
+    return 0;
 }
 
 bool CardputerAdvKeyboard::isModifierKey(uint8_t key)
 {
-    return (key == modifierShiftKey || key == modifierFnKey);
+    return keyToModifierFlag(key) != 0;
 }
 
 #endif
--- src/input/CardputerAdvKeyboard.h
+++ src/input/CardputerAdvKeyboard.h
@@ -5,22 +5,19 @@ class CardputerAdvKeyboard : public TCA8418KeyboardBase
   public:
     CardputerAdvKeyboard();
     void reset(void);
-    void trigger(void) override;
     virtual ~CardputerAdvKeyboard() {}
 
   protected:
     void pressed(uint8_t key) override;
-    void released(void) override;
+    void released(uint8_t key) override;
+    int8_t keyToIndex(uint8_t key);
 
-    void updateModifierFlag(uint8_t key);
+    uint8_t keyToModifierFlag(uint8_t key);
     bool isModifierKey(uint8_t key);
 
   private:
-    uint8_t modifierFlag;        // Flag to indicate if a modifier key is pressed
-    uint32_t last_modifier_time; // Timestamp of the last modifier key press
-    int8_t last_key;
-    int8_t next_key;
-    uint32_t last_tap;
-    uint8_t char_idx;
-    int32_t tap_interval;
+    uint8_t modifierFlag; // Flag to indicate if a modifier key is pressed
+    uint8_t pressedKeysCount;
+    bool onlyOneModifierPressed;
+    bool persistedPreviousModifier;
 };
